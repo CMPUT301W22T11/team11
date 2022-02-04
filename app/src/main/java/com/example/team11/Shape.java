3d21cec56c6ac2a6169764bab95abc76e57c0450
TahiatGoni <tahiatg@gmail.com>
2022-02-03 18:14:08 -0700

    capitalize R for Red

--- app/src/main/java/com/example/team11/Shape.java
+++ app/src/main/java/com/example/team11/Shape.java
@@ -2,7 +2,7 @@ package com.example.team11;
 
 public abstract class Shape {
     private int x, y;
-    String color = "red";
+    String color = "Red";
     public int getX() {
         return x;
     }
