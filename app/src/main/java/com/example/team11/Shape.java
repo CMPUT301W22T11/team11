074eb15cdeab8319a3f194460e8c06df1fb89eca
TahiatGoni <tahiatg@gmail.com>
2022-02-03 18:07:04 -0700

    Conflict of color resolved by changing to red

--- app/src/main/java/com/example/team11/Shape.java
+++ app/src/main/java/com/example/team11/Shape.java
@@ -2,7 +2,7 @@ package com.example.team11;
 
 public abstract class Shape {
     private int x, y;
-    String color = "blue";
+    String color = "red";
     public int getX() {
         return x;
     }
